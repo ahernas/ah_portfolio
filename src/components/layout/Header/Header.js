689102b8eb026a6068f0011c126effb7cce88fb2
Anna Garczarek <ania.garczarek@gmail.com>
2022-03-29 15:00:54 +0200

    Change styles and arrangement of elements in component Header

--- src/components/layout/Header/Header.js
+++ src/components/layout/Header/Header.js
@@ -4,10 +4,10 @@ import styles from './Header.module.scss';
 const Header = () => (
   <div className={styles.root}>
     <div className='container'>
-      <div className={styles.photo}>
-        <div>&lt; coder &gt;</div>
-        <div className={styles.title1}>Hello, I&#39;m Anna</div>
-        <div className={styles.title2}>Front end developer</div>
+      <div className={styles.box}>
+        <div className={styles.title1}>&lt;coder&gt;</div>
+        <div className={styles.title2}>Hello, I&#39;m Anna</div>
+        <div className={styles.title3}>Front end developer</div>
       </div>
     </div>
   </div>
