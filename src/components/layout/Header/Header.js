0fe7a414848e7155a7d9f3d6daf108d01da19aff
Anna Garczarek <ania.garczarek@gmail.com>
2022-03-29 18:23:00 +0200

    Add img to header

--- src/components/layout/Header/Header.js
+++ src/components/layout/Header/Header.js
@@ -3,8 +3,11 @@ import styles from './Header.module.scss';
 
 const Header = () => (
   <div className={styles.root}>
-    <div className='container'>
-      <div className={styles.box}>
+    <div className={'container ' + styles.headerBox}>
+      <div className={styles.imageBox}>
+        <img src='https://i.postimg.cc/xT6gnM7h/image-girl-min.png' alt="image of girl"/>
+      </div>
+      <div className={styles.textBox}>
         <div className={styles.title1}>&lt;coder&gt;</div>
         <div className={styles.title2}>Hello, I&#39;m Anna</div>
         <div className={styles.title3}>Front end developer</div>
