3068e75a36e7581a634d3a8d0479d5eba58b3b80
Anna Garczarek <ania.garczarek@gmail.com>
2022-03-30 18:45:30 +0200

    Add image to AboutBox

--- src/components/features/AboutBox/AboutBox.js
+++ src/components/features/AboutBox/AboutBox.js
@@ -4,11 +4,17 @@ import AboutTitle from '../../common/AboutTitle/AboutTitle';
 
 const AboutBox = () => (
   <div className={styles.root}>
-    <div className={'container ' + styles.boxTitle}>
+    <div className='container'>
       <AboutTitle/>
-      <div className={styles.description}>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      <div className={styles.box}>
+        <div className={styles.description}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </div>
+        <div className={styles.image}>
+          <img src='https://i.postimg.cc/k51hCZBL/IMG-20211202-165752-min.jpg' alt="dog in front of the computer "/>
+        </div>
       </div>
+
     </div>
   </div>
 );
