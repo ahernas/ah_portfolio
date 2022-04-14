baec6fe55e11f79cbb0e4c255a22fc4d5be7b409
Anna Garczarek <ania.garczarek@gmail.com>
2022-04-14 18:52:35 +0200

    Create a new component and move code

---
+++ src/components/common/ProjectMyPlants/ProjectMyPlants.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import styles from './ProjectMyPlants.module.scss';
+
+const ProjectMyPlants = () => (
+  <div>
+    <div className={styles.box}>
+      <div className={styles.name}>
+        My plants <br/> Shop website design
+      </div>
+      <div className={'d-flex justify-content-end ' + styles.imageBox}>
+        <img src='https://i.postimg.cc/3wBXgxcs/phone-orange-min.png' alt='Image of project'/>
+      </div>
+
+    </div>
+    <div className={styles.box}>
+      <div className={'d-flex justify-content-start ' + styles.imageBox}>
+        <img src='https://i.postimg.cc/3Ny25Cxh/laptop-orange-min.png' alt='Image of project'/>
+      </div>
+      <div className={styles.description}>
+        <div className={styles.text}>
+          The project was created in order to learn page coding.<br/>Working tools: Git, GitHub, MongoDB, Express, MongoDB Atlas, Mongoose, Heroku
+          Interface building library: React.
+        </div>
+        <div className={styles.link}>
+          <a href='//github.com/ahernas/Shop-myplants'>code</a>
+          <a href='//my-plant.herokuapp.com/'>live link</a>
+        </div>
+      </div>
+
+    </div>
+  </div>
+);
+
+export default ProjectMyPlants;
--- src/components/features/ProjectBox/ProjectBox.js
+++ src/components/features/ProjectBox/ProjectBox.js
@@ -1,37 +1,14 @@
 import React from 'react';
 import styles from './ProjectBox.module.scss';
 import RunningHead from '../../common/RunningHead/RunningHead';
+import ProjectMyPlants from '../../common/ProjectMyPlants/ProjectMyPlants';
 
 
 const ProjectBox = () => (
   <div className={styles.root}>
     <div className='container'>
       <RunningHead className={styles.title} title={'portfolio'}/>
-      <div className={styles.box}>
-        <div className={styles.name}>
-        My plants <br/> Shop website design
-        </div>
-        <div className={'d-flex justify-content-end ' + styles.imageBox}>
-          <img src='https://i.postimg.cc/3wBXgxcs/phone-orange-min.png' alt='Image of project'/>
-        </div>
-
-      </div>
-      <div className={styles.box}>
-        <div className={'d-flex justify-content-start ' + styles.imageBox}>
-          <img src='https://i.postimg.cc/3Ny25Cxh/laptop-orange-min.png' alt='Image of project'/>
-        </div>
-        <div className={styles.description}>
-          <div className={styles.text}>
-            The project was created in order to learn page coding.<br/>Working tools: Git, GitHub, MongoDB, Express, MongoDB Atlas, Mongoose, Heroku
-            Interface building library: React.
-          </div>
-          <div className={styles.link}>
-            <a href='//github.com/ahernas/Shop-myplants'>code</a>
-            <a href='//my-plant.herokuapp.com/'>live link</a>
-          </div>
-        </div>
-
-      </div>
+      <ProjectMyPlants/>
     </div>
   </div>
 );
