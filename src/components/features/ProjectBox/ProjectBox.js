69db0b1e78e6450efea6caa05df6887a1fc5e18f
Anna Garczarek <ania.garczarek@gmail.com>
2022-04-14 13:09:13 +0200

    Add link to hithub and heroku to component ProjectBox

--- src/components/features/ProjectBox/ProjectBox.js
+++ src/components/features/ProjectBox/ProjectBox.js
@@ -11,17 +11,23 @@ const ProjectBox = () => (
         <div className={styles.name}>
         My plants <br/> Shop website design
         </div>
-        <div className={styles.imageBox}>
+        <div className={'d-flex justify-content-end ' + styles.imageBox}>
           <img src='https://i.postimg.cc/3wBXgxcs/phone-orange-min.png' alt='Image of project'/>
         </div>
 
       </div>
       <div className={styles.box}>
-        <div className={styles.imageBox}>
+        <div className={'d-flex justify-content-start ' + styles.imageBox}>
           <img src='https://i.postimg.cc/3Ny25Cxh/laptop-orange-min.png' alt='Image of project'/>
         </div>
         <div className={styles.description}>
+          <div className={styles.text}>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+          </div>
+          <div className={styles.link}>
+            <a href='//github.com/ahernas/Shop-myplants'>github</a>
+            <a href='//my-plant.herokuapp.com/'>heroku</a>
+          </div>
         </div>
 
       </div>
