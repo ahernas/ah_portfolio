7ea13e30fa7b7838f2241734af6e92bbfc7c83ad
Anna Garczarek <ania.garczarek@gmail.com>
2022-06-28 16:09:24 +0200

    Remove code in component Footer

--- src/components/layout/Footer/Footer.js
+++ src/components/layout/Footer/Footer.js
@@ -3,14 +3,6 @@ import PropTypes from 'prop-types';
 
 import styles from './Footer.module.scss';
 
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-// import {
-//   faTwitter,
-//   faFacebookF,
-//   faYoutube,
-//   faLinkedinIn,
-// } from '@fortawesome/free-brands-svg-icons';
-
 const Footer = () => (
   <footer className={styles.root}>
 
@@ -23,30 +15,6 @@ const Footer = () => (
               ©Copyright by Anna Hernas | All Rights Reserved
             </p>
           </div>
-          {/*<div className={'col-sm-6 col-md-4 text-right ' + styles.socialMedia}>*/}
-          {/*  <ul>*/}
-          {/*    <li>*/}
-          {/*      <a href='/#'>*/}
-          {/*        <FontAwesomeIcon icon={faTwitter}>Twitter</FontAwesomeIcon>*/}
-          {/*      </a>*/}
-          {/*    </li>*/}
-          {/*    <li>*/}
-          {/*      <a href='/#'>*/}
-          {/*        <FontAwesomeIcon icon={faFacebookF}>Facebook</FontAwesomeIcon>*/}
-          {/*      </a>*/}
-          {/*    </li>*/}
-          {/*    <li>*/}
-          {/*      <a href='/#'>*/}
-          {/*        <FontAwesomeIcon icon={faYoutube}>YouTube</FontAwesomeIcon>*/}
-          {/*      </a>*/}
-          {/*    </li>*/}
-          {/*    <li>*/}
-          {/*      <a href='/#'>*/}
-          {/*        <FontAwesomeIcon icon={faLinkedinIn}>LinkedIn</FontAwesomeIcon>*/}
-          {/*      </a>*/}
-          {/*    </li>*/}
-          {/*  </ul>*/}
-          {/*</div>*/}
         </div>
       </div>
     </div>
